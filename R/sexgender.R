d62a57db75a749b04bff61b022c4292d56394acb
John Bryant <john.r.bryant@gmail.com>
2023-09-25 20:21:12 +0700

    revised reformat_sex

--- R/sexgender.R
+++ R/sexgender.R
@@ -5,7 +5,8 @@
 #' Reformat a binary sex variable so
 #' that it consists entirely of
 #' values `"Female"`, `"Male"`,
-#' and possibly `NA`.
+#' and possibly `NA`, and any values
+#' included in `except`.
 #'
 #' When parsing labels, `reformat_sex()`
 #' ignores case: `"FEMALE"` and `"fEmAlE"`
@@ -18,26 +19,35 @@
 #' numeric codes (eg `1`, `2`).
 #'
 #' @param x A vector.
+#' @param except Values to exclude when reformatting.
 #' @param factor Whether the return value
 #' should be a factor.
 #' 
 #' @return If `factor` is `TRUE`,
-#' then `reformat_age()` returns a factor
-#' with levels `"Female"`, `"Male"`.
+#' then `reformat_age()` returns a factor;
 #' otherwise it returns a character vector.
 #'
-#' @seealso [age_labels()], [reformat_sex()]
+#' @seealso [age_labels()], [reformat_age()]
 #' 
 #' @examples
 #' reformat_sex(c("F", "female", NA, "MALES"))
+#'
+#' ## values supplied for 'except'
+#' reformat_sex(c("Fem", "Other", "Male", "M"),
+#'              except = c("Other", "Diverse"))
 #'
 #' ## return an ordinary character vector
 #' reformat_sex(c("F", "female", NA, "MALES"),
 #'              factor = FALSE)
 #' @export
-reformat_sex <- function(x, factor = TRUE) {
+reformat_sex <- function(x, except = NULL, factor = TRUE) {
     synonyms_female <- c("females", "female", "fem", "fe", "f", "women", "girls")
     synonyms_male <- c("males", "male", "ma", "m", "men", "boys")
+    has_except <- !is.null(except)
+    if (has_except && !is.vector(except) && !is.factor(except))
+        cli::cli_abort(c("{.arg except} is not a vector or a factor.",
+                         i = "{.arg except} has class {.cls {class(except)}}."))
+    except <- unique(except)
     check_flag(factor)
     is_na <- is.na(x)
     x_new <- gsub("\\s", "", x)
@@ -45,22 +55,28 @@ reformat_sex <- function(x, factor = TRUE) {
     is_female <- x_new %in% synonyms_female
     is_male <- x_new %in% synonyms_male
     is_valid <- is_na | is_female | is_male
+    if (has_except) {
+        is_except <- x %in% except
+        is_valid <- is_valid | is_except
+    }
     i_invalid <- match(FALSE, is_valid, nomatch = 0L)
     if (i_invalid > 0L)
         cli::cli_abort("Can't parse label {.val {x[[i_invalid]]}}.")
-    x_new[is_female] <- "Female"
-    x_new[is_male] <- "Male"
-    x_new[is_na] <- NA
+    x[is_female] <- "Female"
+    x[is_male] <- "Male"
+    x[is_na] <- NA
     if (factor) {
         levels <- c("Female", "Male")
         if (any(is_na))
             levels <- c(levels, NA)
-        factor(x_new,
+        if (has_except)
+            levels <- c(levels, except)
+        factor(x,
                levels = levels,
                exclude = character())
     }
     else
-        x_new
+        x
 }    
     
     
--- tests/testthat/test-sexgender.R
+++ tests/testthat/test-sexgender.R
@@ -33,5 +33,28 @@ test_that("'reformat_sex' works with valid inputs - character, length == 0", {
     expect_identical(ans_obtained, ans_expected)
 })
 
+test_that("'reformat_sex' works with vector except", {
+    x <- c("fem", "MA", "  boys  ", NA, "F", "X", "Y")
+    ans_obtained <- reformat_sex(x, factor = FALSE, except = c("X", "Y"))
+    ans_expected <- c("Female", "Male", "Male", NA, "Female", "X", "Y")
+    expect_identical(ans_obtained, ans_expected)
+})
+
+test_that("'reformat_sex' works with factor except", {
+    x <- c("fem", "MA", "  boys  ", NA, "F", "X", "Y")
+    ans_obtained <- reformat_sex(x, factor = FALSE, except = factor(c("X", "Y")))
+    ans_expected <- c("Female", "Male", "Male", NA, "Female", "X", "Y")
+    expect_identical(ans_obtained, ans_expected)
+})
+
+test_that("'reformat_sex' throws correct error with invalid except", {
+    expect_error(reformat_sex("M", except = lm),
+                 "`except` is not a vector or a factor.")
+})
+
+
+
+
+
 
     
